2ec1e7418ec5f1af199f83ee7a5791470906ec1d
Leo Zurbriggen <leo.zurbriggen@gmail.com>
2024-03-11 11:08:34 +0100

    WIP upgrade data structures

--- src/entity/upgrades.rs
+++ src/entity/upgrades.rs
@@ -1,47 +1,97 @@
 use macroquad::prelude::rand;
 
-use crate::game_data::GameData;
+use crate::{game_data::GameData, items::weapon::Weapon};
+
+pub struct UpgradeDescription {
+    texture_name: String,
+    text_line1: String,
+    text_line2: Option<String>,
+}
+
+impl UpgradeDescription {
+    pub fn new(texture_name: &str, line1: &str) -> Self {
+        Self {
+            texture_name: texture_name.to_string(),
+            text_line1: line1.to_string(),
+            text_line2: None,
+        }
+    }
+
+    pub fn new_with_line2(texture_name: &str, line1: &str, line2: &str) -> Self {
+        Self {
+            texture_name: texture_name.to_string(),
+            text_line1: line1.to_string(),
+            text_line2: Some(line2.to_string()),
+        }
+    }
+}
 
 pub enum Upgrade {
-    MaxHp(u8),
-    Shooter(ShooterUpgrade),
-    MoveSpeed(f32),
+    Item(ItemUpgrade),
+    Weapon(Weapon),
+    WeaponUpgrade(WeaponUpgrade),
+    CommonUpgrade(CommonUpgrade),
 }
 
 impl Upgrade {
-    pub fn description(&self) -> String {
+    pub fn description(&self) -> UpgradeDescription {
         match self {
-            Upgrade::MaxHp(hp) => format!("+ {} HP", hp),
-            Upgrade::Shooter(upgrade) => upgrade.description(),
-            Upgrade::MoveSpeed(speed) => format!("+ {} Move Speed", speed),
+            Upgrade::Item(item) => item.description(),
+            Upgrade::Weapon(weapon) => match weapon {
+                Weapon::Shooter(shooter) => UpgradeDescription::new("Launcher"),
+            },
+            Upgrade::WeaponUpgrade(upgrade) => upgrade.description(),
+            Upgrade::CommonUpgrade(upgrade) => upgrade.description(),
         }
     }
+}
 
-    pub fn is_general_upgrade(&self) -> bool {
+pub enum ItemUpgrade {
+    Hp(f32),
+    AnomalyBig,
+    AnomalySmall,
+}
+
+impl ItemUpgrade {
+    pub fn description(&self) -> UpgradeDescription {
         match self {
-            Upgrade::MaxHp(_) => true,
-            Upgrade::MoveSpeed(_) => true,
-            _ => false,
+            ItemUpgrade::Hp(hp) => UpgradeDescription::new(format!("+ {} HP", hp).as_str()),
+            ItemUpgrade::AnomalyBig => UpgradeDescription::new_with_line2("- 50%", "Aberration"),
+            ItemUpgrade::AnomalySmall => UpgradeDescription::new_with_line2("- 10%", "Aberration"),
         }
     }
+}
 
-    pub fn is_shoot_upgrade(&self) -> bool {
+pub enum CommonUpgrade {
+    MaxHp(u8),
+    MoveSpeed(f32),
+}
+
+impl CommonUpgrade {
+    pub fn description(&self) -> UpgradeDescription {
         match self {
-            Upgrade::Shooter(_) => true,
-            _ => false,
+            CommonUpgrade::MaxHp(hp) => {
+                UpgradeDescription::new_with_line2(format!("+ {}", hp).as_str(), "Max HP")
+            }
+            CommonUpgrade::MoveSpeed(speed) => {
+                UpgradeDescription::new_with_line2(format!("+ {}%", speed).as_str(), "Move Speed")
+            }
         }
     }
+}
 
-    pub fn len() -> usize {
-        3
-    }
+pub enum WeaponUpgrade {
+    Shooter(ShooterUpgrade),
+    Balls(BallsUpgrade),
+    Dash(DashUpgrade),
+}
 
-    pub fn generate_by_index(index: usize) -> Self {
-        match index {
-            0 => Upgrade::MaxHp(1),
-            1 => Upgrade::MoveSpeed(10.),
-            2 => Upgrade::Shooter(ShooterUpgrade::generate_by_index(index)),
-            _ => unreachable!(),
+impl WeaponUpgrade {
+    pub fn description(&self) -> UpgradeDescription {
+        match self {
+            WeaponUpgrade::Shooter(upgrade) => upgrade.description(),
+            WeaponUpgrade::Balls(upgrade) => upgrade.description(),
+            WeaponUpgrade::Dash(upgrade) => upgrade.description(),
         }
     }
 }
@@ -50,22 +100,36 @@ pub enum ShooterUpgrade {
     FireRate(f32),
 }
 
+pub enum BallsUpgrade {
+    Amount(usize),
+}
+
+pub enum DashUpgrade {}
+
 impl ShooterUpgrade {
-    pub fn description(&self) -> String {
+    pub fn description(&self) -> UpgradeDescription {
         match self {
-            ShooterUpgrade::FireRate(rate) => format!("+ {} Fire Rate", rate),
+            ShooterUpgrade::FireRate(rate) => {
+                UpgradeDescription::new_with_line2(format!("+ {}%", rate).as_str(), "Fire Rate")
+            }
         }
     }
+}
 
-    pub fn len() -> usize {
-        1
-    }
-
-    pub fn generate_by_index(index: usize) -> Self {
-        match index {
-            0 => ShooterUpgrade::FireRate(5.),
-            _ => unreachable!(),
+impl BallsUpgrade {
+    pub fn description(&self) -> UpgradeDescription {
+        match self {
+            BallsUpgrade::Amount(amount) => {
+                UpgradeDescription::new_with_line2(format!("+ {}", amount).as_str(), "Projectiles")
+            }
         }
+    }
+}
+
+impl DashUpgrade {
+    pub fn description(&self) -> UpgradeDescription {
+        // TODO
+        UpgradeDescription::new("")
     }
 }
 
